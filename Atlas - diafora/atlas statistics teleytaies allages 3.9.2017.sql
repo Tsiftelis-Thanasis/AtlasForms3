5832af964f85e290d2f2503bbfa15ee281dff17e
Thanasis Tsiftelis <tsiftelis.thanasis@gmail.com>
2017-09-16 17:04:11 +0300

    Διάφορα...

---
+++ Atlas - diafora/atlas statistics teleytaies allages 3.9.2017.sql
@@ -0,0 +1,15 @@
+Select * from DiorganwshTable d
+where DiorganwshName = 'йупекко'
+Where d.Id in (11)
+                    
+
+Copy and then the new ids!
+					
+					
+ConfigSessionid = 4
+        
+PlayoffsIds = 4,9,15
+      
+SLPlayoffsIds = 5,10,16
+
+CupIds = 11, 17
